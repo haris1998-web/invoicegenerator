73c96d6f13ce92a9c102731484cef9eed651dfc1
Haris <muhammadharisyousaf@gmail.com>
2024-04-06 15:42:19 +0500

    Updates

---
+++ precache-manifest.5a3541623a603ce353510cb1dc26fa13.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cfbef64cbb013a90635333ae77f1f6d",
+    "url": "/react-invoice-generator/index.html"
+  },
+  {
+    "revision": "b62458e1565ce7a011b1",
+    "url": "/react-invoice-generator/static/css/2.950e20d2.chunk.css"
+  },
+  {
+    "revision": "1759955360c3a4d43763",
+    "url": "/react-invoice-generator/static/css/main.239c0d14.chunk.css"
+  },
+  {
+    "revision": "b62458e1565ce7a011b1",
+    "url": "/react-invoice-generator/static/js/2.f8a2b8de.chunk.js"
+  },
+  {
+    "revision": "5fae47d0a016a5b68e2af829b08bec2a",
+    "url": "/react-invoice-generator/static/js/2.f8a2b8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1759955360c3a4d43763",
+    "url": "/react-invoice-generator/static/js/main.73a76e72.chunk.js"
+  },
+  {
+    "revision": "aabb303fe90f96d72bbb",
+    "url": "/react-invoice-generator/static/js/runtime-main.33c79cdc.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/react-invoice-generator/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "15c01d9b81ceb6702aa92e3b5416ce50",
+    "url": "/react-invoice-generator/static/media/remove.15c01d9b.svg"
+  },
+  {
+    "revision": "d4da359f65106dae13fb51495f035419",
+    "url": "/react-invoice-generator/static/media/resize.d4da359f.svg"
+  }
+]);
